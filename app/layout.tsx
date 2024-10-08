b38960d6d97cdbef26bba027716ace794cfcd919
golosoman <constan.lapin2015@mail.ru>
2024-10-08 17:21:00 +0100

    add checkout page, add action for email, cokkies e.t.d.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import { Nunito } from 'next/font/google';
+import { Nunito } from "next/font/google";
 
-import './globals.css';
-import { Providers } from '@/components/shared/providers';
+import "./globals.css";
+import { Providers } from "@/shared/components/shared/providers";
 
 const nunito = Nunito({
-  subsets: ['cyrillic'],
-  variable: '--font-nunito',
-  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ["cyrillic"],
+  variable: "--font-nunito",
+  weight: ["400", "500", "600", "700", "800", "900"],
 });
 
 export default function RootLayout({
